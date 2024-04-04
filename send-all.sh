22cae066dbb0495d6fb959f3d516680f92f07645
Adam Chasen <adam@chasen.name>
2024-04-03 23:54:35 -0400

    switch from ampy to mpremote

--- send-all.sh
+++ send-all.sh
@@ -1,16 +1,18 @@
-for FILE in boot.py main.py ssd1306.py senko.py wifimgr.py
-do
-	ampy put ${FILE}
-done
 #cd python_lcd/
 #ampy put lcd
 #cd -
 cd micropython-ndeflib/src/
-ampy put ndef
+mpremote ${MPREMOTE_PORT} cp -r ndef :
 cd -
 cd Adafruit_CircuitPython_PN532/
-ampy put adafruit_pn532
+mpremote ${MPREMOTE_PORT} cp -r adafruit_pn532 :
 cd -
 cd micropython-spotify-web-api/
-ampy put spotify_web_api
+mpremote ${MPREMOTE_PORT} cp -r spotify_web_api :
 cd -
+# There is a setup process which stalls "boot.py". mpremote appears to soft reset after uploading and does not interrupt boot.py
+# add boot file last
+for FILE in boot.py main.py ssd1306.py senko.py wifimgr.py
+do
+    mpremote ${MPREMOTE_PORT} cp ${FILE} :
+done
