e2aabcbad7d70d4411d79fcca4ddbaa89e94fcea
Adam Chasen <adam@chasen.name>
2025-01-24 13:44:18 -0500

    modify send-all script to assume libraries are frozen into firmware image

--- send-all.sh
+++ send-all.sh
@@ -1,15 +1,16 @@
 #cd python_lcd/
 #ampy put lcd
 #cd -
-cd micropython-ndeflib/src/
-mpremote ${MPREMOTE_PORT} cp -r ndef :
-cd -
-cd Adafruit_CircuitPython_PN532/
-mpremote ${MPREMOTE_PORT} cp -r adafruit_pn532 :
-cd -
-cd micropython-spotify-web-api/
-mpremote ${MPREMOTE_PORT} cp -r spotify_web_api :
-cd -
+#don't copy these over because they are frozen using manifest.py now
+#cd micropython-ndeflib/src/
+#mpremote ${MPREMOTE_PORT} cp -r ndef :
+#cd -
+#cd Adafruit_CircuitPython_PN532/
+#mpremote ${MPREMOTE_PORT} cp -r adafruit_pn532 :
+#cd -
+#cd micropython-spotify-web-api/
+#mpremote ${MPREMOTE_PORT} cp -r spotify_web_api :
+#cd -
 # There is a setup process which stalls "boot.py". mpremote appears to soft reset after uploading and does not interrupt boot.py
 # add boot file last
 for FILE in boot.py main.py ssd1306.py senko.py wifimgr.py
